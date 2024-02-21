e977ef143a4c2bf06a53624facb8bd65a16631d2
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-02-21 11:34:46 +0100

    faker api mit fetch eingebunden

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function Card({ name, title, imageUri }) {
   return (
-    <div className="border">
-      <h2>{name}</h2>
-      <h4>{title}</h4>
+    <div className="border flex h-40">
+      <div className="flex-1">
+        <h2>{name}</h2>
+        <h4>{title}</h4>
+      </div>
 
       <div>
-        <img src={imageUri} alt=""></img>
+        <img src={imageUri} alt="" className="object-cover w-full h-full"></img>
       </div>
     </div>
   );
--- src/components/PeopleContainer.js
+++ src/components/PeopleContainer.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "components/Card";
 
 export default function PeopleContainer() {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch("https://65d5ccbcf6967ba8e3bc9d2d.mockapi.io/People").then((res) =>
+      res.json().then((data) => {
+        setPeople(data);
+      })
+    );
+  }, []);
+
   return (
     <div>
       <h1>People</h1>
-      <div>
-        <Card
-          name="hans"
-          title="ceo"
-          imageUri="https://picsum.photos/200/300"
-        />
+      <div className="w-40">
+        {people.map(person => {
+          return (
+            <Card
+              name={person.name}
+              imageUri={person.avatar}
+              title={person.jobtitle}
+            />
+        })}
       </div>
     </div>
   );
