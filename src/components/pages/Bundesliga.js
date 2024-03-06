5de72c66f3d5f7af80d3b92eacd19f63c686dae5
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-03-06 10:44:07 +0100

    Begonnen mit Football

--- src/components/pages/Bundesliga.js
+++ src/components/pages/Bundesliga.js
@@ -4,9 +4,9 @@ import background from "../atoms/images/background.jpg";
 
 export default function Bundesliga() {
   return (
-    <div>
-      <div className="absolute -z-50">
-        <img className="bg-contain" src={background} alt="Background" />
+    <div className="no-scrollbar">
+      <div className="absolute -z-50 object-cover ">
+        <img className="h-screen w-screen" src={background} alt="Background" />
       </div>
       <div className="text-white">
         <h1>Bundesliga</h1>
--- src/components/pages/Table.js
+++ src/components/pages/Table.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import background from "../atoms/images/background.jpg";
 
 export default function Bundesliga() {
   return (
-    <div>
-      <h1>Table</h1>
-      <Link to="/">Bundesliga</Link>
+    <div className="no-scrollbar">
+      <div className="absolute -z-50 object-cover ">
+        <img className="h-screen w-screen" src={background} alt="Background" />
+      </div>
+      <div className="text-white">
+        <h1>Table</h1>
+        <Link className="" to="/">
+          Bundesliga
+        </Link>
+      </div>
     </div>
   );
 }
