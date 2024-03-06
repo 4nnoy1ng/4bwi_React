9910cbfc0377aa5efd704a4b151fc39ca0e6cdbe
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2024-03-06 13:28:40 +0100

    Bundesligaseite Fertig

--- src/components/pages/Bundesliga.js
+++ src/components/pages/Bundesliga.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import background from "../atoms/images/background.jpg";
-import styles from "./Bundesliga.css";
+import styles from "./Bundesliga.module.css";
 
 export default function Bundesliga() {
   return (
@@ -9,10 +9,22 @@ export default function Bundesliga() {
       <div className="absolute -z-50 object-cover ">
         <img className="h-screen w-screen" src={background} alt="Background" />
       </div>
-      <div className={styles.title}> Bundes</div>
-      <div className="text-white "> Liga</div>
+      <div className={styles.header}>
+        <div className={styles.title}> Bundes</div>
+        <div className={styles.subtitle}> Liga</div>
+        <div className={styles.source}></div>
+      </div>
+
+      <div className={styles.footballclubs}>
+        <div className={styles.RedBullSalzburg}></div>
+        <div className={styles.Graz}></div>
+        <div className={styles.LASK}></div>
+        <div className={styles.Rapid}></div>
+        <div className={styles.Wien}></div>
+        <div className={styles.Klagenfurt}></div>
+      </div>
       <div>
-        <Link className="text-white" to="/Table">
+        <Link className={styles.Tabellen} to="/Table">
           Table
         </Link>
       </div>
--- src/components/pages/Table.js
+++ src/components/pages/Table.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import background from "../atoms/images/background.jpg";
-import styles from "./Bundesliga.css";
+import styles from "./Tabellen.module.css";
 
 export default function Bundesliga() {
   return (
@@ -9,12 +9,35 @@ export default function Bundesliga() {
       <div className="absolute -z-50 object-cover ">
         <img className="h-screen w-screen" src={background} alt="Background" />
       </div>
-      <div className="text-white">
-        <h1>Table</h1>
-        <Link className="" to="/">
-          Bundesliga
-        </Link>
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <div className={styles.source}>ADMIRALBundesliga</div>
+          <div className={styles.date}>Season 2023/24</div>
+        </div>
+
+        <div className={styles.SecondHeader}>
+          <div className={styles.Verein}>Verein</div>
+          <div className={styles.Games}> Spiele</div>
+          <div className={styles.Sieg}>S</div>
+          <div className={styles.Unentschieden}>U</div>
+          <div className={styles.Niederlage}>N</div>
+          <div className={styles.Tore}>Tore</div>
+          <div className={styles.Punkte}>Punkte</div>
+        </div>
+
+        <div className={styles.SecondHeader}>
+          <div className={styles.Verein}>Verein</div>
+          <div className={styles.Games}> Spiele</div>
+          <div className={styles.Sieg}>S</div>
+          <div className={styles.Unentschieden}>U</div>
+          <div className={styles.Niederlage}>N</div>
+          <div className={styles.Tore}>Tore</div>
+          <div className={styles.Punkte}>Punkte</div>
+        </div>
       </div>
+      <Link className="" to="/">
+        Bundesliga
+      </Link>
     </div>
   );
 }
